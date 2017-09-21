7dcaf1652db9b3dd9d8a542a1cc78ab587ea42be
Charlie Dill <charlie_dill@Charlies-MacBook-Pro-2.local>
2017-09-20 20:54:24 -0500

    tried submit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-import rotate_string from caesar.py
+from flask import Flask, request, redirect
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG']= True
@@ -28,23 +28,30 @@ form="""
         </head>
         <body>
         <!--Create your text here-->
-        <form action="submit" id="textform" method="post">
+        <form action="submit" id="textform" methods="post">
         <label for= "rot">Rotate by:</label> 
         <input id= "rot" type="text" id= "rot" name="rot"></input>
-        <textarea name="text"></textarea>
+        <textarea name="text">Type text here!</textarea>
         <input type="submit" id="rot" name="rot">
 
         </body>
     </html>
 """
 
-@app.route("/")#make only posts
+@app.route("/") #, method=['POST'])
 def blank_form():
-    return form 
+    return form
+
+#@app.route('/submit')
+#def encrypt():
+    #
+
 
 @app.route("/submit")
-def encrypt(rotate_string(text, rot)):
-    #first_name = request.form['first_name']
-    return '<h1>Hi there</h1>'
+def encrypt():
+    text = request.form['text']
+    rot = request.form['rot']
+    message =rotate_string(text,rot)
+    return message
 
 app.run()
